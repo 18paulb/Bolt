5ebe44b9fef55b0d5fb179a6ce2af003eab90764
Brandon <bjpaul99@gmail.com>
2023-12-09 16:04:35 -0700

    forgot to add this

---
+++ boltfrontend/src/app/template-browser/template-browser.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {lastValueFrom} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {ReviewTemplate} from "../../../../shared/models/ReviewTemplate";
+
+@Component({
+  selector: 'app-template-browser',
+  templateUrl: './template-browser.component.html',
+  styleUrls: ['./template-browser.component.css']
+})
+export class TemplateBrowserComponent implements OnInit {
+
+  constructor(private http:HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.getReviewTemplates().catch(console.dir);
+  }
+
+  reviewTemplates: ReviewTemplate[] = []
+
+  async getReviewTemplates() {
+    let results:any = await lastValueFrom(this.http.get("http://localhost:3000/getReviewTemplates/"+"brandon"))
+    console.log(results)
+    for (let i = 0; i < results.length; ++i) {
+      let result = results[i]
+      this.reviewTemplates.push(new ReviewTemplate(result["imageUrl"], result["messageText"], result["messageDescription"], result["suggestedResponses"]))
+    }
+  }
+
+  async sendReviewTemplate(template:ReviewTemplate) {
+    await lastValueFrom(this.http.post("http://localhost:3000/sendReviewTemplate", template))
+  }
+}
